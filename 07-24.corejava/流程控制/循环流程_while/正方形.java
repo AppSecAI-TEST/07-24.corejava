5456f9809627f5801b4c12c92775392ca5e7513c
cofcoltd <cofcoltd@qq.com>
2017-08-04 17:24:34 +0800

    空心　正方形

---
+++ 07-24.corejava/流程控制/循环流程_while/正方形.java
@@ -0,0 +1,22 @@
+package 循环流程_while;
+
+public class 正方形 {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 20; i++) {
+			System.out.print("×");
+
+			for (int j = 1; j <= 20; j++) {
+				if(i==1 || i==20 || j ==20)
+					System.out.print("×");
+				else {
+					System.out.print("√");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
