8ca8f11fb7c4c79d3d78bf2f6c52068b5efc8035
cofcoltd <cofcoltd@qq.com>
2017-08-11 11:29:43 +0800

    案例.在线考试系统-01

---
+++ 07-24.corejava/阶段案例/案例_选择题案例/Answer.java
@@ -0,0 +1,25 @@
+package 案例_选择题案例;
+
+public class Answer {
+	
+	private char[] choice;
+	
+	public Answer() {}
+
+	public Answer(char[] choice) {
+		super();
+		this.choice = choice;
+	}
+
+	public char[] getChoice() {
+		return choice;
+	}
+
+	public void setChoice(char[] choice) {
+		this.choice = choice;
+	}
+
+	
+	
+	
+}
